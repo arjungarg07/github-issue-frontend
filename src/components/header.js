94b21ebbd6328b24cc95560d418c85b4261f7b13
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-10-31 03:18:07 +0530

    feat: increase font size

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Header = () => <h1>GitHub Issue Demo</h1>
+const Header = () => <h1><div className="text-3xl">GitHub Issue Demo</div></h1>
 
 export default Header;
